515a7e26bc4de75da9ab6e2d604a5ecb7c592b93
mikay <mikay@mikayoshi32@gmail.com>
2025-08-23 19:57:52 +0700

    Update Catch2 version to v3.9.0 and fix FetchContent_MakeAvailable target name

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -13,6 +13,6 @@ include(FetchContent)
 FetchContent_Declare(
     catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-    GIT_TAG        v3.0.0
+    GIT_TAG        v3.9.0
 )   
-FetchContent_MakeAvailable(catch2)
+FetchContent_MakeAvailable(Catch2)
