fd815bf900b284fb8db7011731e1439c47eb5f2c
mikay <mikay@mikayoshi32@gmail.com>
2025-08-25 04:45:30 +0700

    update clang-tidy

--- src/library.cpp
+++ src/library.cpp
@@ -1,6 +1,9 @@
+#ifndef CMAKE_TEMPLATE_HEADER_ONLY
+
 #include <cmake-template/library.hpp>
 
-auto factorial(int n) noexcept -> int
+[[nodiscard]] auto factorial(int n) noexcept -> int
 {
     return factorial_inline(n);
 }
+#endif
