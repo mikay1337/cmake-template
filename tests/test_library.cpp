3476b68e4442654aab62dde859f7d5ab154b7da2
mikay <mikay@mikayoshi32@gmail.com>
2025-08-26 16:09:24 +0700

    fix: invalid test logic

--- tests/test_library.cpp
+++ tests/test_library.cpp
@@ -7,7 +7,7 @@ TEST_CASE("TestLibrary", "[math]")
   REQUIRE(Factorial(5) == 120);
   REQUIRE(FactorialInline(5) == 120);
   REQUIRE(FactorialConstexpr(5) == 120);
-  REQUIRE(Factorial(-1) == -1);
+  REQUIRE(Factorial(-1) == 1);
   REQUIRE(FactorialInline(1) == 1);
   REQUIRE(FactorialInline(0) == 1);
   TestTreeFunction();
